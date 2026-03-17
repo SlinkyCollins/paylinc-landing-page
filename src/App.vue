755d55dafc16a91bbcf4757bf7ebdb85b0a53e10
Ademola Afolabi <afolabiademola27@gmail.com>
2026-03-17 21:55:41 +0100

    Add Tailwind CSS integration and update dependencies

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 
 <template>
   <h1>You did it!</h1>
+  <h1 class="text-3xl font-bold underline bg-blue-500">
+    Hello Tailwind CSS v4.2!
+  </h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
