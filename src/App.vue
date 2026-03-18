1faa4f9404ca68002fc4021bb9a0d0368a35d6d9
Ademola Afolabi <afolabiademola27@gmail.com>
2026-03-18 14:38:06 +0100

    Add GSAP and Lucide Vue Next dependencies, restructure App.vue, and create Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <h1 class="text-3xl font-bold underline bg-blue-500">
-    Hello Tailwind CSS v4.2!
-  </h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
 </template>
 
 <style scoped></style>
