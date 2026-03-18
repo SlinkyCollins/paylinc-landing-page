5373e15c7745e314cba049a915a4d57dcf942344
Ademola Afolabi <afolabiademola27@gmail.com>
2026-03-18 15:07:52 +0100

    Refactor App.vue to include new components and create initial structure for Navbar, Footer, Hero, PaymentApps, Problem, Process, Audience, Trust, and Signup components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <script setup>
-
+import Audience from './components/Audience.vue';
+import FloatingButton from './components/FloatingButton.vue';
+import Footer from './components/Footer.vue';
+import Hero from './components/Hero.vue';
+import Navbar from './components/Navbar.vue';
+import PaymentApps from './components/PaymentApps.vue';
+import Problem from './components/Problem.vue';
+import Process from './components/Process.vue';
+import Signup from './components/Signup.vue';
+import Trust from './components/Trust.vue';
 </script>
 
 <template>
+  <div class="min-h-screen bg-white">
+    <Navbar />
+    <main>
+      <Hero />
+      <PaymentApps />
+      <Problem />
+      <Process />
+      <Audience />
+      <Trust />
+      <Signup />
+    </main>
+    <Footer />
+    <FloatingButton />
+  </div>
 </template>
-
-<style scoped></style>
